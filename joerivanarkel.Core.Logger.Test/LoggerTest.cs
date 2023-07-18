422af3a4acc90465beebcd078fea876aed599112
joerivanarkel <Joeri@vanarkel.net>
2023-07-18 23:39:14 +0200

    new icon

--- joerivanarkel.Core.Logger.Test/LoggerTest.cs
+++ joerivanarkel.Core.Logger.Test/LoggerTest.cs
@@ -3,6 +3,7 @@ using joerivanarkel.Core.Logger;
 using joerivanarkel.Core.Logger.FileHandlers;
 using joerivanarkel.Core.Logger.Interfaces;
 using joerivanarkel.Core.Logger.FileHandlers.Model;
+using joerivanarkel.Core.Logger.Exception;
 using Xunit;
 using static joerivanarkel.Core.Logger.Logger;
 
@@ -45,5 +46,27 @@ public class LoggerTest
         // Assert
         Assert.True(result);
     }
-        
+
+    [Fact]
+    public void ShouldNotLog()
+    {
+        try
+        {
+            // Arrange
+            var FileWriteHandler = A.Fake<IFileWriteHandler>();
+            var logger = new Logger();
+
+            A.CallTo(() => FileWriteHandler.WriteToFile(A<FileWriteModel>.Ignored, A<bool>.Ignored)).Returns(false);
+            var message = "";
+            var type = LogType.INFO;
+            // Act
+            var result = logger.Log(message, type);
+        }
+        catch (System.Exception ex)
+        {
+            Assert.IsType<LoggerException>(ex);
+            Assert.Equal("Message cannot be null or empty", ex.Message);
+        }
+    }
+
 }
