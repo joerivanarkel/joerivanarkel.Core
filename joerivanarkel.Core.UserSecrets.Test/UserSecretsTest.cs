1896cbd26c3c57577f754a3fea0bc66591ff16d1
joerivanarkel <Joeri@vanarkel.net>
2023-07-18 19:59:58 +0200

    UserSecret tests

---
+++ joerivanarkel.Core.UserSecrets.Test/UserSecretsTest.cs
@@ -0,0 +1,37 @@
+using joerivanarkel.Core.UserSecrets;
+using joerivanarkel.Core.UserSecrets.Exception;
+using Microsoft.Extensions.Configuration;
+
+namespace joerivanarkel.Core.UserSecrets.Test;
+
+public class UserSecretsTest
+{
+    [Fact]
+    // Ensure that the secret is set in the environment variables
+    // Otherwise run the following command in the test project directory:
+    // dotnet user-secrets init
+    // dotnet user-secrets set Test 12345
+    public void GetSecret_WithValidSecretName_ReturnsSecretValue()
+    {
+        // Arrange
+        string secretName = "Test";
+        string secretValue = "12345";
+
+        // Act
+        string result = UserSecrets<UserSecretsTest>.GetSecret(secretName);
+
+        // Assert
+        Assert.Equal(secretValue, result);
+    }
+
+    [Fact]
+    public void GetSecret_WithInvalidSecretName_ThrowsUserSecretException()
+    {
+        // Arrange
+        string secretName = "InvalidSecret";
+
+        // Act & Assert
+        var ex = Assert.Throws<UserSecretException>(() => UserSecrets<UserSecretsTest>.GetSecret(secretName));
+        Assert.Equal($"Could not get secret {secretName} from user secrets. The secret is null or empty", ex.Message);
+    }
+}
--- joerivanarkel.Core.UserSecrets/UserSecrets.cs
+++ joerivanarkel.Core.UserSecrets/UserSecrets.cs
@@ -13,27 +13,18 @@ namespace joerivanarkel.Core.UserSecrets
                 var secretConfig = new ConfigurationBuilder()
                     .AddUserSecrets<T>()
                     .Build();
+
+                if (secretConfig[secretName] == null || secretConfig[secretName] == string.Empty) throw new UserSecretException($"Could not get secret {secretName} from user secrets. The secret is null or empty");
+                
                 return secretConfig[secretName];
+            }
+            catch (UserSecretException exception)
+            {
+                throw exception;
             }
             catch (System.Exception)
             {
                 throw new UserSecretException($"Could not get secret {secretName} from user secrets");
-            }
-        }
-
-        public static bool AddSecret(string key, string value)
-        {
-            try
-            {
-                var secretConfig = new ConfigurationBuilder()
-                    .AddUserSecrets<T>()
-                    .Build();
-                secretConfig[key] = value;
-                return true;
-            }
-            catch (System.Exception)
-            {
-                throw new UserSecretException($"Could not add secret {key} to user secrets");
             }
         }
 
