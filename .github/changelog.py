330ab5ed3f552fd01eef34ab6c18dc72499f44e3
joerivanarkel <Joeri@vanarkel.net>
2023-08-06 21:11:27 +0200

    changelog update

--- .github/changelog.py
+++ .github/changelog.py
@@ -5,15 +5,12 @@ import getopt
 # Build Number | ${{ github.run_number }} | --build-number
 
 build_number = 0
-
-# Commit | ${{ github.sha }} | --commit
 commit = ""
-
-# Commit Message | ${{ github.event.head_commit.message }} | --commit-message
 commit_message = ""
-
-# Commit Author | ${{ github.event.head_commit.author.name }} | --commit-author
 commit_author = ""
+modified_files = []
+added_files = []
+removed_files = []
 
 
 def extract_value(argv, arg):
@@ -35,8 +32,26 @@ def extract_value(argv, arg):
     return variable
 
 
+def extract_value_list(argv, arg):
+    index = argv.index(arg) + 1
+    
+    variable = [argv[index]]
+    
+    index += 1
+    
+    if index >= len(argv):
+        return variable
+
+    while argv[index][0] != "-":
+        variable.append(argv[index])
+        index += 1
+        if index >= len(argv):
+            break
+        
+    return variable
+
 def get_variables(argv):
-    global build_number, commit, commit_message, commit_author
+    global build_number, commit, commit_message, commit_author, modified_files, added_files, removed_files
 
     # Parse command line arguments
 
@@ -49,21 +64,33 @@ def get_variables(argv):
             commit_message = extract_value(argv, arg)
         elif arg == "--commit-author":
             commit_author = extract_value(argv, arg)
+        elif arg == "--modified-files":
+            modified_files = extract_value_list(argv, arg)
+        elif arg == "--added-files":
+            added_files = extract_value_list(argv, arg)
+        elif arg == "--removed-files":
+            removed_files = extract_value_list(argv, arg)
+            
 
     # Print variables
 
 def write_to_changelog():
-    global build_number, commit, commit_message, commit_author
+    global build_number, commit, commit_message, commit_author, modified_files, added_files, removed_files
 
     # Write to changelog
     # open("CHANGELOG.md", "x")
     
+    new_line = "<br>\n"
+    
     with open("doc/CHANGELOG.md", "a") as changelog:
-        changelog.write("## Build " + build_number + "\n")
-        changelog.write("Commit: " + commit + "\n")
-        changelog.write("Commit Message: " + commit_message + "\n")
-        changelog.write("Commit Author: " + commit_author + "\n")
-        changelog.write("\n")
+        changelog.write("## Build " + build_number + new_line)
+        changelog.write("Commit: " + commit + new_line)
+        changelog.write("Commit Message: " + commit_message + new_line)
+        changelog.write("Commit Author: " + commit_author + new_line)
+        changelog.write("Modified Files: " + str(modified_files) + new_line)
+        changelog.write("Added Files: " + str(added_files) + new_line)
+        changelog.write("Removed Files: " + str(removed_files) + new_line)
+        changelog.write(new_line)
         
     
         
